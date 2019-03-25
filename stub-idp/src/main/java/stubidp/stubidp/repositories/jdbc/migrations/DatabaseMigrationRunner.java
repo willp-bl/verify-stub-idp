16f979b562a751acb308577aa741b4b6aedc615e
William Palmer <willpdp@users.noreply.github.com>
2019-03-25 20:45:48 +0000

    [stub-idp] update flyway
    closes #7

--- stub-idp/src/main/java/stubidp/stubidp/repositories/jdbc/migrations/DatabaseMigrationRunner.java
+++ stub-idp/src/main/java/stubidp/stubidp/repositories/jdbc/migrations/DatabaseMigrationRunner.java
@@ -5,11 +5,12 @@ import org.flywaydb.core.Flyway;
 public class DatabaseMigrationRunner {
 
     public void runMigration(String dbUrl) {
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(dbUrl, null, null);
-        flyway.setBaselineVersionAsString("0");
-        flyway.setBaselineOnMigrate(true);
-        flyway.setLocations("classpath:db.migrations.common", getDBSpecificMigration(dbUrl));
+        Flyway flyway = Flyway.configure()
+                .dataSource(dbUrl, null, null)
+                .baselineVersion("0")
+                .baselineOnMigrate(true)
+                .locations("classpath:db.migrations.common", getDBSpecificMigration(dbUrl))
+                .load();
 
         flyway.migrate();
     }
