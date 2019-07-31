060bc3eaf1e13653dca479a11f4347b4c606d14c
William Palmer <willp-bl@users.noreply.github.com>
2019-08-01 00:06:25 +0100

    [stub-idp] stop using deprecated code

--- stub-idp/src/test/java/stubidp/stubidp/resources/EidasProxyNodeServiceMetadataResourceTest.java
+++ stub-idp/src/test/java/stubidp/stubidp/resources/EidasProxyNodeServiceMetadataResourceTest.java
@@ -25,7 +25,7 @@ import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.text.MessageFormat;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
