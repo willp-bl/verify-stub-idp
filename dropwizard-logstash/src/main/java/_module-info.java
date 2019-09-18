7df02a7e28f0a51778945049bad2cd0e1bba256a
William Palmer <willpdp@users.noreply.github.com>
2019-09-18 20:35:45 +0100

    update to latest dropwizard 2 rc, still got jpms issues

--- dropwizard-logstash/src/main/java/_module-info.java
+++ dropwizard-logstash/src/main/java/_module-info.java
@@ -1,17 +1,18 @@
-/*
-    dropwizard.logging contains classes in a package also in logback.core
-    https://github.com/dropwizard/dropwizard/issues/2312
-*/
 //@SuppressWarnings({"requires-automatic", "requires-transitive-automatic"})
 //module dropwizard.logstash {
-//    requires dropwizard.core;
-//    requires org.joda.time;
+//
+//    opens stubidp.dropwizard.logstash; // for tests
+//
+//    requires dropwizard.core; // has package conflict with dropwizard.validation in io.dropwizard.validation
 //    requires dropwizard.logging;
+//    requires dropwizard.validation;
+//    requires org.joda.time;
 //    requires logback.access;
 //    requires jackson.annotations;
 //    requires com.fasterxml.jackson.core;
+//    requires com.fasterxml.jackson.databind;
 //    requires logstash.logback.encoder;
 //    requires logback.core;
 //    requires logback.classic;
-//    requires validation.api;
+//    requires java.validation;
 //}
