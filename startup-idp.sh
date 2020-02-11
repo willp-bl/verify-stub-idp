91ba0607594befb230b7b4e5fe0b6ef8cdc0b576
William Palmer <willpdp@users.noreply.github.com>
2020-02-11 23:15:42 +0000

    [dist] update startup scripts with external_host config

--- startup-idp.sh
+++ startup-idp.sh
@@ -5,6 +5,6 @@ set -e
 # for quick testing use an in-memory db
 export DB_URI="jdbc:h2:mem:stubidpdb;MODE=PostgreSQL;DB_CLOSE_DELAY=-1"
 # configure the host that is externally visible for users here - note no path
-export EXPECTED_DESTINATION="http://localhost"
+export EXPECTED_DESTINATION="http://localhost:50140"
 
 java -jar stub-idp-*.jar server configuration/stub-idp.yml
--- startup-sp.sh
+++ startup-sp.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+# configure the host that is externally visible for users here - note no path
+export EXPECTED_DESTINATION="http://localhost:40000"
+
 java -jar stub-sp-*.jar server configuration/stub-sp.yml
