ce58ed4f64a235f41b01993e3e88b79591dbedd0
William Palmer <willpdp@users.noreply.github.com>
2020-03-10 20:06:27 +0000

    [saml-serializers] changes required for shibboleth library update

--- saml-serializers/src/main/java/stubidp/saml/serializers/deserializers/validators/Base64StringDecoder.java
+++ saml-serializers/src/main/java/stubidp/saml/serializers/deserializers/validators/Base64StringDecoder.java
@@ -1,23 +1,28 @@
 package stubidp.saml.serializers.deserializers.validators;
 
 import net.shibboleth.utilities.java.support.codec.Base64Support;
+import net.shibboleth.utilities.java.support.codec.DecodingException;
 import org.apache.commons.codec.binary.StringUtils;
 import stubidp.saml.extensions.validation.SamlTransformationErrorException;
 import stubidp.saml.extensions.validation.SamlValidationSpecificationFailure;
 
-import static java.util.regex.Pattern.matches;
+import java.util.regex.Pattern;
+
 import static stubidp.saml.serializers.errors.SamlTransformationErrorFactory.invalidBase64Encoding;
 
 public class Base64StringDecoder {
 
     public String decode(String input) {
-        String withoutWhitespace = input.replaceAll("\\s", "");
-        if (!matches("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$", withoutWhitespace)) {
-            SamlValidationSpecificationFailure failure = invalidBase64Encoding(input);
-            throw new SamlTransformationErrorException(failure.getErrorMessage(), failure.getLogLevel());
+        final String withoutWhitespace = input.replaceAll("\\s", "");
+        if (Pattern.matches("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$", withoutWhitespace)) {
+            try {
+                return StringUtils.newStringUtf8(Base64Support.decode(input));
+            } catch (DecodingException e) {
+                // fall through and throw a SamlTransformationErrorException exception
+            }
         }
-
-        return StringUtils.newStringUtf8(Base64Support.decode(input));
+        SamlValidationSpecificationFailure failure = invalidBase64Encoding(input);
+        throw new SamlTransformationErrorException(failure.getErrorMessage(), failure.getLogLevel());
     }
 
 }
--- saml-serializers/src/main/java/stubidp/saml/serializers/serializers/XmlObjectToBase64EncodedStringTransformer.java
+++ saml-serializers/src/main/java/stubidp/saml/serializers/serializers/XmlObjectToBase64EncodedStringTransformer.java
@@ -1,20 +1,27 @@
 package stubidp.saml.serializers.serializers;
 
 import net.shibboleth.utilities.java.support.codec.Base64Support;
+import net.shibboleth.utilities.java.support.codec.EncodingException;
 import net.shibboleth.utilities.java.support.xml.SerializeSupport;
 import org.apache.commons.codec.binary.StringUtils;
 import org.opensaml.core.xml.XMLObject;
+import org.slf4j.event.Level;
 import org.w3c.dom.Element;
+import stubidp.saml.extensions.validation.SamlTransformationErrorException;
 
 import java.util.function.Function;
 
-public class XmlObjectToBase64EncodedStringTransformer<TInput extends XMLObject> implements Function<TInput,String> {
+public class XmlObjectToBase64EncodedStringTransformer<TInput extends XMLObject> implements Function<TInput, String> {
 
     @Override
     public String apply(XMLObject signableXMLObject) {
         Element signedElement = marshallToElement(signableXMLObject);
         String node = SerializeSupport.nodeToString(signedElement);
-        return Base64Support.encode(StringUtils.getBytesUtf8(node), Base64Support.UNCHUNKED);
+        try {
+            return Base64Support.encode(StringUtils.getBytesUtf8(node), Base64Support.UNCHUNKED);
+        } catch (EncodingException e) {
+            throw new SamlTransformationErrorException(e.getMessage(), Level.ERROR);
+        }
     }
 
     private static Element marshallToElement(XMLObject rootObject) {
