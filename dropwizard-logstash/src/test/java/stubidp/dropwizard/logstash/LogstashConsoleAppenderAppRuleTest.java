bfdbd6f3f87bd40d04a7ecb955972f9737d700f8
William Palmer <willpdp@users.noreply.github.com>
2020-03-23 20:49:54 +0000

    [dropwizard-logstash] increase test reliability

--- dropwizard-logstash/src/test/java/stubidp/dropwizard/logstash/LogstashConsoleAppenderAppRuleTest.java
+++ dropwizard-logstash/src/test/java/stubidp/dropwizard/logstash/LogstashConsoleAppenderAppRuleTest.java
@@ -16,13 +16,13 @@ import stubidp.dropwizard.logstash.support.TestConfiguration;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static stubidp.dropwizard.logstash.support.RootResource.HELLO_MESSAGE;
 import static stubidp.dropwizard.logstash.support.RootResource.TEST_LOG_LINE;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
@@ -30,21 +30,23 @@ public class LogstashConsoleAppenderAppRuleTest {
 
     public static DropwizardAppExtension<TestConfiguration> dropwizardAppRule = new DropwizardAppExtension<>(TestApplication.class, ResourceHelpers.resourceFilePath("console-appender-test-application.yml"));
 
+    private final Client client = new JerseyClientBuilder().build();
+
     @Test
     @CaptureSystemOutput
-    public void testLoggingLogstashRequestLog(CaptureSystemOutput.OutputCapture outputCapture) throws InterruptedException, IOException {
-        Client client = new JerseyClientBuilder().build();
+    public void testLoggingLogstashRequestLog(CaptureSystemOutput.OutputCapture outputCapture) {
+        final String REFERER = "http://foobar/";
+        final String USER_AGENT = "lynx/1.337";
+        final String URL_PATH = "/?queryparam=test";
 
-        for(int i=0;i<5;i++) {
-            final Response response = client.target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/?queryparam=test").request()
-                    .header("Referer", "http://foobar/").header("User-Agent", "lynx/1.337").get();
-            assertThat(response.readEntity(String.class)).isEqualTo("hello!");
+        for(int i = 0; i<5; i++) {
+            final Response response = client.target("http://localhost:" + dropwizardAppRule.getLocalPort() + URL_PATH)
+                    .request()
+                    .header("Referer", REFERER)
+                    .header("User-Agent", USER_AGENT)
+                    .get();
+            assertThat(response.readEntity(String.class)).isEqualTo(HELLO_MESSAGE);
         }
-        // If we try to read systemOutRule too quickly, under some circumstances the appender won't have
-        // successfully written the expected access log line yet.  We haven't got to the bottom of this
-        // but it seems to depend on whether another DropwizardAppRule test has been run before this one.
-        // sleeping for a while fixes the problem
-        Thread.sleep(500);
 
         final List<AccessEventFormat> list = parseLogsOfType(AccessEventFormat.class, outputCapture);
 
@@ -52,36 +54,29 @@ public class LogstashConsoleAppenderAppRuleTest {
         assertThat(accessEventStream.size()).as("check there's an access log in the following:\n%s", List.of(outputCapture.toString().split(System.lineSeparator()))).isGreaterThanOrEqualTo(1);
         AccessEventFormat accessEvent = accessEventStream.get(0);
         assertThat(accessEvent.getMethod()).isEqualTo("GET");
-        assertThat(accessEvent.getReferer()).isEqualTo("http://foobar/");
-        assertThat(accessEvent.getUserAgent()).isEqualTo("lynx/1.337");
+        assertThat(accessEvent.getReferer()).isEqualTo(REFERER);
+        assertThat(accessEvent.getUserAgent()).isEqualTo(USER_AGENT);
         assertThat(accessEvent.getHost()).startsWith("localhost");
-        assertThat(accessEvent.getBytesSent()).isEqualTo("hello!".length());
-        assertThat(accessEvent.getUrl()).isEqualTo("/?queryparam=test");
+        assertThat(accessEvent.getBytesSent()).isEqualTo(HELLO_MESSAGE.length());
+        assertThat(accessEvent.getUrl()).isEqualTo(URL_PATH);
         assertThat(accessEvent.getHttpVersion()).isEqualTo("1.1");
         assertThat(accessEvent.getResponseCode()).isEqualTo(200);
         assertThat(accessEvent.getRemoteIp()).isEqualTo("127.0.0.1");
         assertThat(accessEvent.getVersion()).isEqualTo(1);
         // ballpark check that the unit is in the right order of magnitude
         // this test should hopefully catch a value that's erroneously measured in seconds
-        assertThat(accessEvent.getElapsedTimeMillis()).isBetween(3,3000);
+        assertThat(accessEvent.getElapsedTimeMillis()).isBetween(1,4000);
     }
 
     @Test
     @CaptureSystemOutput
-    public void testRequestLogWithMissingRefererHeader(CaptureSystemOutput.OutputCapture outputCapture) throws InterruptedException, IOException {
-        Client client = new JerseyClientBuilder().build();
-
+    public void testRequestLogWithMissingRefererHeader(CaptureSystemOutput.OutputCapture outputCapture) {
         for(int i=0;i<5;i++) {
-            final Response response = client.target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/").request()
+            final Response response = client.target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/")
+                    .request()
                     .get();
             assertThat(response.readEntity(String.class)).isEqualTo("hello!");
         }
-
-        // If we try to read systemOutRule too quickly, under some circumstances the appender won't have
-        // successfully written the expected access log line yet.  We haven't got to the bottom of this
-        // but it seems to depend on whether another DropwizardAppRule test has been run before this one.
-        // sleeping for a while fixes the problem
-        Thread.sleep(500);
 
         final List<AccessEventFormat> list = parseLogsOfType(AccessEventFormat.class, outputCapture);
 
@@ -91,26 +86,25 @@ public class LogstashConsoleAppenderAppRuleTest {
         assertThat(accessEvent.getReferer()).isEqualTo("-");
     }
 
-
     @Test
     @CaptureSystemOutput
-    public void testLoggingLogstashFileLog(CaptureSystemOutput.OutputCapture outputCapture) throws IOException, InterruptedException {
-
-        Client client = new JerseyClientBuilder().build();
-
-        final Response response = client.target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/log").request()
-                .get();
+    public void testLoggingLogstashFileLog(CaptureSystemOutput.OutputCapture outputCapture) {
+        for(int i=0;i<5;i++) {
+            final Response response = client.target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/log")
+                    .request()
+                    .get();
+            assertThat(response.getStatus()).isEqualTo(204);
+        }
 
         final List<LoggingEventFormat> list = parseLogsOfType(LoggingEventFormat.class, outputCapture);
 
-        assertThat(list.size()).isEqualTo(1);
-
+        assertThat(list.size()).isGreaterThanOrEqualTo(1);
         assertThat(list.stream()
                 .filter(logFormat -> logFormat.getMessage().equals(TEST_LOG_LINE))
-                .count()).isEqualTo(1);
+                .count()).isGreaterThanOrEqualTo(1);
     }
 
-    private <E> List<E> parseLogsOfType(Class<E> logType, CaptureSystemOutput.OutputCapture outputCapture) throws IOException {
+    private <E> List<E> parseLogsOfType(Class<E> logType, CaptureSystemOutput.OutputCapture outputCapture) {
         ObjectMapper objectMapper = new ObjectMapper();
         List<E> list = new ArrayList<>();
         StringTokenizer stringTokenizer = new StringTokenizer(outputCapture.toString(), System.lineSeparator());
--- dropwizard-logstash/src/test/java/stubidp/dropwizard/logstash/support/RootResource.java
+++ dropwizard-logstash/src/test/java/stubidp/dropwizard/logstash/support/RootResource.java
@@ -8,12 +8,14 @@ import javax.ws.rs.Path;
 @Path("/")
 public class RootResource {
     private static final Logger LOG = Logger.getLogger(RootResource.class);
-    public static String TEST_LOG_LINE = "test log line";
+    public static final String TEST_LOG_LINE = "test log line";
+    public static final String HELLO_MESSAGE = "hello!";
 
     @GET
     public String get() {
-        return "hello!";
+        return HELLO_MESSAGE;
     }
+
     @GET
     @Path("log")
     public void log() {
